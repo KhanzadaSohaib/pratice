2a5c932715a6542e1e688fb8681b371bb2d16e27
Sohaib <shobiikhanzada@gmail.com>
2023-09-08 11:05:15 +0500

    JavaScript pratice

---
+++ modules1.js
@@ -0,0 +1,8 @@
+export let name = 'Sohaib Ahmed';
+
+export function hello() {
+}
+
+export class User{
+
+}
---
+++ modules2.js
@@ -0,0 +1,12 @@
+import {name} from './modules1'
+
+
+import {hello} from './modules1'
+
+import {user} from './modules1'
+
+console.log(name)
+
+hello()
+
+let a = new user();
